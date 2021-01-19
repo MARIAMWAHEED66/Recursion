18bd0d7a2f6cfdbf7cbd5d39c05d333630a228d0
mariamwaheed66 <m_7maid@hotmail.com>
2021-01-19 08:46:14 +0300

    third commit

--- Recursion/recursion.js
+++ Recursion/recursion.js
@@ -4,6 +4,7 @@ const numbers = [
   [4, 2, [6, 7, [2, 6, 1]]],
 ];
 
+// const numbers = [1, 2, 3, 4, 5];
 // let sum = 0;
 // for (let num of numbers) sum += num;
 // console.log(sum);
@@ -12,11 +13,12 @@ function sum(array) {
   return array.length === 0 ? 0 : array.reduce((sum, num) => sum + num, 0);
 }
 
-// const numbers = [1, 2, 3, 4, 5];
 console.log(sum(numbers));
 
-// const sum = function (num) {
-//   if (num.length < 1) return 0;
-//   return num.pop() + sum(num);
-// };
-// console.log(sum(numbers));
+for (let i = 0; i < numbers.length; i++) {
+  const sum = function (num) {
+    if (num.length < 1) return 0;
+    return num.pop() + sum(num);
+  };
+}
+console.log(sum(numbers));
